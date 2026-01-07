d76e55421dbbb2cc6ffbde9eb676f17add75767a
mwihoti <danielmwihoti@gmail.com>
2026-01-07 03:33:30 +0300

    update lite-cert logo and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,19 +3,18 @@ import "./globals.css";
 import { Providers } from "./providers";
 
 export const metadata: Metadata = {
-  title: "Lovable App",
-  description: "Lovable Generated Project",
+  title: "Lite-Cert",
+  description: "lite-cert",
   authors: [{ name: "Lovable" }],
   openGraph: {
-    title: "Lovable App",
+    title: "Lite-Cert",
     description: "Lovable Generated Project",
     type: "website",
-    images: ["https://lovable.dev/opengraph-image-p98pqg.png"],
+   
   },
   twitter: {
     card: "summary_large_image",
     site: "@Lovable",
-    images: ["https://lovable.dev/opengraph-image-p98pqg.png"],
   },
 };
 
